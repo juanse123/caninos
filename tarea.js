2926c50611360a5a70ca00e904c0d1ca84934a2e
todex11 <juansekpte18@gmail.com>
2019-06-10 11:58:27 -0500

    mi maquetacion

---
+++ tarea.js
@@ -0,0 +1,28 @@
+// var titulo1;
+// var titulo2;
+// var titulo3;
+// var texto1;
+// var texto2;
+// var texto3;
+
+//  titulo1 = prompt("escriba un titulo", " ");
+//  	texto1 = prompt("escriba un texto", " ");
+//  titulo2 = prompt("escriba un titulo", " ");
+//  	 texto2 = prompt("escriba un texto", " ");
+//  titulo3 = prompt("escriba un titulo", " ");
+// 	texto3 = prompt("escriba un texto", " ");
+
+// document.getElementById('tit1').innerHTML= titulo1;
+// document.getElementById('tit2').innerHTML= titulo2;
+// document.getElementById('tit3').innerHTML= titulo3;
+
+// document.getElementById('tex1').innerHTML= texto1;
+// document.getElementById('tex2').innerHTML= texto2;
+// document.getElementById('tex3').innerHTML= texto3;
+
+
+document.getElementById('tit1').innerHTML='cuidados para sus mascotas'
+document.getElementById('tex1').innerHTML='ofrecemos asistencia medica,y cencelnedjncnlnlnlencldnd'
+
+document.getElementById('tit2').innerHTML='educacion para mascotas '
+document.getElementById('tex2').innerHTML='ara las mascotas mas dificiles de manejar'
